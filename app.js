f10498922ed42848d67f1858e1d5a32ae4fc4927
yanpin0524 <a131234568@gmail.com>
2022-06-01 16:21:33 +0800

    fix: 刪掉斜線

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const server = http.createServer(app)
 const { Server } = require('socket.io')
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:8080/',
+    origin: 'http://localhost:8080',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
