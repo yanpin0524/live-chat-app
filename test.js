5799503cfc976460dd9ac6ae978c41c3f28ee08a
KateChu <chu.shihchien@gmail.com>
2022-06-01 21:55:32 +0800

    interrupt for open a rabbitMQ branch

--- test.js
+++ test.js
@@ -6,19 +6,21 @@ const Broker = require('./src/services/rabbitMQ')
 
 const app = express()
 
-app.use(async (req, res, next) => {
-  try {
-    const RMQProducer = await new Broker().init()
-    console.log('===== test process.env.NODE_ENV =====', process.env.NODE_ENV)
-    // we now have access to rabbitMQ
-    next()
-  } catch (error) {
-    process.exit(1)
-  }
-})
-app.use((req, res, next) => {
-  next(creatError.NotFound())
-})
+console.log('===== outside app.use test =====', process.env.RABBITMQ_URL)
+const RMQProducer = new Broker().init()
+// app.use(async (req, res, next) => {
+//   try {
+//     console.log('===== inside app.use test =====', process.env.RABBITMQ_URL)
+//     const RMQProducer = await new Broker().init()
+//     // we now have access to rabbitMQ
+//     next()
+//   } catch (error) {
+//     process.exit(1)
+//   }
+// })
+// app.use((req, res, next) => {
+//   next(creatError.NotFound())
+// })
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('server is running', process.env.PORT || 3000)
