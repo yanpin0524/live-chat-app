ca8fee0319b9471cdb5b10ce36f60804c38ffa99
KateChu <chu.shihchien@gmail.com>
2022-06-10 11:00:12 +0800

    feat: heroku-redis

--- utilities/cache.js
+++ utilities/cache.js
@@ -1,5 +1,10 @@
 const Redis = require('redis')
-const redisClient = Redis.createClient()
+const port = process.env.PORT || 6379
+const client = {
+  host: process.env.REDIS_URL || '127.0.0.1',
+  port: port
+}
+const redisClient = Redis.createClient(client)
 const DEFAULT_EXPIRATION = 3600
 
 module.exports = function getOrSetCache (key, cb) {
